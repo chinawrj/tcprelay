332b8567cdf6b281f190f1fbe114284dc4b29411
Rujun Wang <chinawrj@gmail.com>
2016-09-21 23:10:23 +0800

    log record support

--- log.c
+++ log.c
@@ -37,16 +37,53 @@ static int log_dmsg_is_whitelist(char *log)
 int log_handle(char *log)
 {
     uint32_t ip_src[4], ip_dst[4];
-    uint32_t timestamp;
+    unsigned long long timestamp;
     uint32_t port;
-    uint32_t count;
     char *str;
     int ret;
 
     if (log_dmsg_is_whitelist(log)) {
         printf("white list\n");
     } else if (log_dmsg_is_blacklist(log)) {
+        /*
+        4,1813,131759525162,-;ipsetmagic_blacklistIN= OUT=ens34 SRC=192.168.231.130 DST=115.239.210.27 LEN=60 TOS=0x00 PREC=0x00 TTL=64 ID=16302 DF PROTO=TCP SPT=49900 DPT=80 WINDOW=29200 RES=0x00 SYN URGP=0
+        */
         printf("black list\n");
+        ret = sscanf(log, "%*u,%*u,%llu\n", &timestamp);
+        if (1 != ret) {
+            return -1;
+        }
+        printf("timestamp found %llu ", timestamp);
+        str = strstr(log, "SRC=");
+        if (NULL == str) {
+            return -1;
+        }
+        printf("SRC found ");
+        ret = sscanf(str, "SRC=%u.%u.%u.%u", &(ip_src[0]), &(ip_src[1]), &(ip_src[2]), &(ip_src[3]));
+        if (4 != ret) {
+            return -1;
+        }
+        printf("%u.%u.%u.%u, ", ip_src[0], ip_src[1], ip_src[2], ip_src[3]);
+        str = strstr(log, "DST=");
+        if (NULL == str) {
+            return -1;
+        }
+        printf("DST found ");
+        ret = sscanf(str, "DST=%u.%u.%u.%u", &(ip_dst[0]), &(ip_dst[1]), &(ip_dst[2]), &(ip_dst[3]));
+        if (4 != ret) {
+            return -1;
+        }
+        printf("%u.%u.%u.%u, ", ip_dst[0], ip_dst[1], ip_dst[2], ip_dst[3]);
+        str = strstr(log, "DPT=");
+        if (NULL == str) {
+            return -1;
+        }
+        printf("DPT found ");
+        ret = sscanf(str, "DPT=%u", &port);
+        if (1 != ret) {
+            return -1;
+        }
+        printf("%u\n", port);
     }
     return 0;
 }
